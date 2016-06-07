de23c6d86ac08f9ce87c685335d96ba84d3a2d84
FaArIsH <faarishhassan@gmail.com>
2016-06-07 19:48:14 +0500

    Update Android.mk

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,27 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 # HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
-ifeq ($(BOARD_HAVE_GENERIC_BLN),true)
-    LOCAL_CFLAGS += -DGENERIC_BLN
-endif
-
-ifeq ($(BOARD_HAVE_MULTI_COLOR_LED),true)
-    LOCAL_CFLAGS += -DMULTI_COLOR_LED
-endif
-
 LOCAL_SRC_FILES := lights.c
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog
-
-LOCAL_MODULE := lights.msm8960
-
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
