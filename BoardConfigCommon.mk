d7ef54c916d59e0697f0aaf202c194e14c16b6b2
FaArIsH <faarishhassan@gmail.com>
2016-07-13 14:16:06 +0500

    Enable Legacy MMAP

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,5 +102,7 @@ TARGET_NEED_DISABLE_FACE_DETECTION_BOTH_CAMERAS := true
 COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
 BOARD_RIL_CLASS := ../../../device/samsung/lt02ltexx-common/ril/
 
+# Legacy MMAP for pre-lollipop blobs
+BOARD_USES_LEGACY_MMAP := true
 # CMHW
 BOARD_HARDWARE_CLASS := device/samsung/lt02ltexx-common/cmhw
