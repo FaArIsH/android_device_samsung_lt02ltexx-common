a59df0787a40f318226a2a53bb0c3e427a7ec406
FaArIsH <faarishhassan@gmail.com>
2016-07-18 20:48:47 +0500

    Update lt02ltexx-common.mk

--- lt02ltexx-common.mk
+++ lt02ltexx-common.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/lt02ltexx-common/lt02ltexx-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/wilcoxltexx/wilcoxltexx-vendor.mk)
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02ltexx-common/overlay
@@ -187,4 +187,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
