347d90b4ac0a834afed236caea1889753da68c93
FaArIsH <faarishhassan@gmail.com>
2017-02-17 23:36:40 +0500

    first commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,78 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from common msm8930
+-include device/samsung/msm8930-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/lt02ltexx-common/include
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8930-common
+TARGET_KERNEL_CONFIG := cyanogenmod_lt02ltexx_defconfig
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/lt02ltexx-common/rootdir/fstab.qcom
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5821660160
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/lt02ltexx-common/bluetooth
+
+# Audio
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Allow suspend in charge mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# Camera
+TARGET_NEED_DISABLE_AUTOFOCUS := true
+TARGET_NEED_DISABLE_FACE_DETECTION := true
+TARGET_NEED_DISABLE_FACE_DETECTION_BOTH_CAMERAS := true
+
+# RIL
+TARGET_RIL_VARIANT := caf
+BOARD_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
+BOARD_RIL_CLASS := ../../../device/samsung/lt02ltexx-common/ril/
+
+# Time service
+BOARD_USES_QC_TIME_SERVICES := true
+
+# CMHW
+BOARD_HARDWARE_CLASS += device/samsung/lt02ltexx-common/cmhw
---
+++ system_prop.mk
@@ -0,0 +1,58 @@
+# Set composition for USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=SerranoRIL \
+    ro.ril.telephony.mqanelements=6 \
+    persist.radio.add_power_save=1
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qmienabled=true \
+    persist.gps.qc_nlp_in_use=1 \
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x0 \
+    ro.gps.agps_provider=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.disable=1 \
+    mm.enable.smoothstreaming=true \
+    use.dedicated.device.for.voip=true \
+    use.voice.path.for.pcm.voip=true \
+    media.aac_51_output_enabled=true \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
+	persist.bluetooth.disableabsvol=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
+    ro.sf.lcd_density=160 \
+    ro.opengles.version=196608 \
+    ro.qualcomm.cabl=0 \
+    ro.hwui.text_large_cache_height=2048
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=MSM8930AB \
+    ro.product_ship=true \
+    ro.warmboot.capability=1 \
+    persist.timed.enable=true \
+    wifi.interface=wlan0 \
+    ro.qualcomm.bt.hci_transport=smd \
+    qcom.bluetooth.soc=smd
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false \
+    ro.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=4
